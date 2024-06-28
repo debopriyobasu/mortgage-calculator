1ed8b39d85bfcabbcdb643a5d394dbdca85f77ef
Debopriyo Basu <debopriyobasu@gmail.com>
2024-06-28 13:58:22 +0530

    chore: vite setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import MortgageCalculator from "./components/MortgageCalculator";
+import Results from "./components/Results";
+
+function App() {
+  return (
+    <>
+      <MortgageCalculator />
+      <Results />
+    </>
+  );
+}
+
+export default App;
